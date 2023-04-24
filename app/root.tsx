bac108ef2fa5e59a63c99f2ab3ebdde6a0f10762
Sasha Moak <sasha.moak@gmail.com>
2023-04-23 21:53:59 -0700

    feat: upgrade remix and use new v2 routes

--- app/root.tsx
+++ app/root.tsx
@@ -6,21 +6,28 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import type { MetaFunction, LinksFunction } from "@remix-run/node";
+import type { V2_MetaFunction, LinksFunction } from "@remix-run/node";
 import styles from "./tailwind.css";
 
-export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "NHL App",
-  viewport: "width=device-width,initial-scale=1",
-});
+export const meta: V2_MetaFunction = () => {
+  return [
+    {
+      title: "NHL App",
+    },
+  ];
+};
 
-export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles },
+  { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+];
 
 export default function App() {
   return (
     <html lang="en">
       <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
       </head>
