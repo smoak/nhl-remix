0cf95fbc3229df73a220962e5007dc53ecd281d6
Sasha Moak <sasha.moak@gmail.com>
2023-10-12 22:55:06 -0700

    feat: conditionally show the playoff nav link
    When the regular season has not ended yet, we do not want to show the
    playoff nav link. This way it will only show up once the regular season
    has ended.

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,5 +1,14 @@
 import { Link, NavLink } from "@remix-run/react";
 import { NHL } from "~/components/NHLLogos/NHL";
+import { isEndOfRegularSeason } from "~/date-fns";
+
+const PlayoffNavLink = () => {
+  if (!isEndOfRegularSeason()) {
+    return null;
+  }
+
+  return <NavLink to="/playoffs">Playoffs</NavLink>;
+};
 
 export const Header = () => (
   <header className="container mx-auto flex items-center justify-between">
@@ -9,7 +18,7 @@ export const Header = () => (
     <nav className="flex gap-6">
       <NavLink to="/">Home</NavLink>
       <NavLink to="/standings">Standings</NavLink>
-      <NavLink to="/playoffs">Playoffs</NavLink>
+      <PlayoffNavLink />
     </nav>
   </header>
 );
--- app/date-fns/index.ts
+++ app/date-fns/index.ts
@@ -1,9 +1,13 @@
-import { getHours, startOfDay, subDays } from "date-fns";
+import { compareAsc, getHours, startOfDay, subDays } from "date-fns";
 import { utcToZonedTime } from "date-fns-tz";
 
 export const EST_IANA_ZONE_ID = "America/New_York";
 export const DATE_LINK_FORMAT = "yyyy-MM-dd";
 export const DATE_DISPLAY_FORMAT = "dd MMMM yyyy";
+export const REGULAR_SEASON_END_MONTH = 3; // April
+export const REGULAR_SEASON_END_DAY = 14;
+export const PLAYOFF_END_MONTH = 6; // July
+export const PLAYOFF_END_DAY = 1; // small buffer because sometimes playoffs go long
 
 const getTimeZonedDay = (date: Date): Date => {
   const hours = getHours(date);
@@ -20,4 +24,22 @@ export const getToday = () => {
   const etNow = utcToZonedTime(now, EST_IANA_ZONE_ID);
 
   return getTimeZonedDay(etNow);
+};
+
+export const isEndOfRegularSeason = () => {
+  const today = getToday();
+  const seasonEnd = new Date(
+    today.getFullYear(),
+    REGULAR_SEASON_END_MONTH,
+    REGULAR_SEASON_END_DAY
+  );
+  const playoffEnd = new Date(
+    today.getFullYear(),
+    PLAYOFF_END_MONTH,
+    PLAYOFF_END_DAY
+  );
+
+  return (
+    compareAsc(today, seasonEnd) === 1 && compareAsc(today, playoffEnd) === -1
+  );
 };
