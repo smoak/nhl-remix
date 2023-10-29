fd5537c77d3cd609d0e4e8b7c65e07efe1d3e3e9
Sasha Moak <sasha.moak@gmail.com>
2023-10-28 22:42:31 -0700

    chore: add storybook with initial config and single story

---
+++ .storybook/main.ts
@@ -0,0 +1,29 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+import { mergeConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+const config: StorybookConfig = {
+  stories: [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      plugins: [tsconfigPaths()],
+    });
+  },
+};
+export default config;
---
+++ stories/TeamLogo.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { TeamLogo } from "~/components/TeamLogo";
+
+const meta: Meta<typeof TeamLogo> = {
+  component: TeamLogo,
+  argTypes: {
+    teamId: {
+      options: [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+        22, 23, 24, 25, 26, 28, 29, 30, 52, 53, 54, 55,
+      ],
+      control: { type: "select" },
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof TeamLogo>;
+
+export const Default: Story = {
+  args: {
+    size: 48,
+    teamAbbreviation: "VAN",
+  },
+};
