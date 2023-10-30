f43d547caf86b122db24c68a04123a0c8848cd5d
Sasha Moak <sasha.moak@gmail.com>
2023-10-29 19:10:04 -0700

    chore: add Teamname story

---
+++ stories/TeamName.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { TeamName } from "~/components/TeamName";
+
+const meta: Meta<typeof TeamName> = {
+  component: TeamName,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof TeamName>;
+
+export const Default: Story = {
+  args: {
+    isGameInProgress: false,
+    isGoaliePulled: false,
+    isOnPowerPlay: false,
+    name: "Canucks",
+  },
+};
